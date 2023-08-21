f18b8a958027e9f9752b507f4029f376a1fe3f37
Joachim1103 <111863407+Joachim1103@users.noreply.github.com>
2023-08-21 10:47:45 +0200

    Something

--- Compulsory 1/Compulsory 1/Source.cpp
+++ Compulsory 1/Compulsory 1/Source.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 
+//using namespace std;
+//
+//int fact(int n) {
+//	if ((n == 0) || (n == 1))
+//		return 1;
+//	else
+//		return n * fact(n - 1);
+//}
+//int main() {
+//	int n = 5;
+//	cout << "Factorial of " << n << " is " << fact(n);
+//	return 0;
+//}
+
 using namespace std;
-int main() {
-	int n = 5, fact = 1, i;
-	for (i = 1; i <= n; i++)
-		fact = fact * i;
-	cout << "Factorial of " << n << " is " << fact;
+int f(int x)
+{
+	if ((x == 1) || (x == 0)) 
+	{
+		return(x);
+	}
+	else 
+	{
+		return(f(x - 1) + f(x - 2));
+	}
+}
+int main()
+{
+	int x, i = 0;
+	cout << "Number : ";
+	cin >> x;
+	cout << "\nFibonnaci : ";
+	while (i < x) 
+	{
+		cout << " " << f(i);
+		i++;
+	}
 	return 0;
 }
